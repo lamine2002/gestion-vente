104ca5ffa5f1db71f7e22eaa90e9de81ea76b901
Mohamed Lamine NIANG <123456789+odin@users.noreply.github.com>
2024-02-19 12:09:53 +0000

    Creation des tables

--- database/migrations/2024_02_19_114222_create_products_table.php
+++ database/migrations/2024_02_19_114222_create_products_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role')->default('user');
-            $table->string('photo')->nullable();
-            $table->rememberToken();
+            $table->string('description');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->string('photo');
+            $table->foreignId('category_id')->constrained('categories');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
