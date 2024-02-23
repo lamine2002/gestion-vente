0e41bf4657ffdadcc62e5fca3a9d33b2b10226f2
Mohamed Lamine NIANG <123456789+odin@users.noreply.github.com>
2024-02-23 22:58:18 +0000

    ajout de  = [] pour mes differentes modeles

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//Route::get('/admin/dashboard', function () {
+//    return view('admin.dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -38,6 +38,11 @@ Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('/dashboard', function () {
         return view('admin.dashboard');
     })->name('dashboard');
+
+    Route::resource('users', \App\Http\Controllers\Admin\UserController::class)->except(['show']);
+    Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class)->except(['show']);
+    Route::resource('customers', \App\Http\Controllers\Admin\CustomerController::class)->except(['show']);
+    Route::resource('products', \App\Http\Controllers\Admin\ProductController::class)->except(['show']);
 });
 
 require __DIR__.'/auth.php';
