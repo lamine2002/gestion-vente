ae18283c10a4614f5d8b9010817c8bcf8e4f004b
Mohamed Lamine NIANG <123456789+odin@users.noreply.github.com>
2024-02-25 13:30:45 +0000

    implementation de la CategoryController

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\CategoryFormRequest;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -12,7 +14,11 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.categories.index',
+            [
+                'categories' => \App\Models\Category::all()
+            ]
+        );
     }
 
     /**
@@ -20,46 +26,57 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.categories.form',
+            [
+                'category' => new \App\Models\Category()
+            ]
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CategoryFormRequest $request)
     {
-        //
+        $category = Category::create($request->validated());
+        return redirect()->route('admin.categories.index')->with('success', "La catégorie $category->name a été créée avec succès");
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
-    }
+//    public function show(string $id)
+//    {
+//        //
+//    }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Category $category)
     {
-        //
+        return view('admin.categories.form',
+            [
+                'category' => $category
+            ]
+        );
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CategoryFormRequest $request, Category $category)
     {
-        //
+        $category->update($request->validated());
+        return redirect()->route('admin.categories.index')->with('success', "La catégorie $category->name a été modifiée avec succès");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return redirect()->route('admin.categories.index')->with('success', "La catégorie $category->name a été supprimée avec succès");
     }
 }
