054e736bb56008e31d18474d25d0e403b9203549
Mohamed Lamine NIANG <123456789+odin@users.noreply.github.com>
2024-02-26 15:34:25 +0000

    Implementatin de UserFormRequest

--- app/Http/Requests/Admin/UserFormRequest.php
+++ app/Http/Requests/Admin/UserFormRequest.php
@@ -11,7 +11,7 @@ class UserFormRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class UserFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'],
+            'role' => ['required', 'string'],
+            'photo' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
         ];
     }
 }
