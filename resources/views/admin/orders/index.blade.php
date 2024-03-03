8c89a9193ba4fac6570073256b65da2ba8e34a4e
Mohamed Lamine NIANG <123456789+odin@users.noreply.github.com>
2024-03-03 17:05:10 +0000

    Ajout de couleur sur les status de index de order

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -7,6 +7,9 @@
         <a href="{{ route('admin.orders.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ajouter une Commande</a>
     </div>
 
+
+
+{{-- de joli card et des couleurs en fonctions du status de la commande     --}}
     <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -50,7 +53,17 @@
                                             </div>
                                         </td>
                                         <td class="px-6 py-4 whitespace-no-wrap">
-                                            <div class="text-sm leading-5 text-gray-900">{{ $order->status }}</div>
+                                            <div class="text-sm leading-5 text-gray-900">
+                                                @if($order->status == 'En attente')
+                                                    <span class="bg-yellow-200 text-yellow-600 py-1 px-3 rounded-full text-xs">En attente</span>
+                                                @elseif($order->status == 'En cours de traitement')
+                                                    <span class="bg-blue-200 text-blue-600 py-1 px-3 rounded-full text-xs">En cours de traitement</span>
+                                                @elseif($order->status == 'Terminée')
+                                                    <span class="bg-green-200 text-green-600 py-1 px-3 rounded-full text-xs">Terminée</span>
+                                                @elseif($order->status == 'Annulée')
+                                                    <span class="bg-red-200 text-red-600 py-1 px-3 rounded-full text-xs">Annulée</span>
+                                                @endif
+                                            </div>
                                         </td>
                                         <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
                                             <a href="{{ route('admin.orders.show', $order) }}" class="text-indigo-600 hover:text-indigo-900 mr-4">Voir</a>
@@ -68,12 +81,13 @@
                             @endforeach
                         </tbody>
                     </table>
-
-
                 </div>
             </div>
+
         </div>
     </div>
+
+
 
     <div class="mt-4">
         {{ $orders->links() }}
