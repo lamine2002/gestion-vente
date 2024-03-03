0cda80cefa663b0997c19a7beaf05aa0e112ffe1
Mohamed Lamine NIANG <123456789+odin@users.noreply.github.com>
2024-03-03 17:16:25 +0000

    Mise a jour des autorisations de OrderPolicy

--- app/Policies/OrderPolicy.php
+++ app/Policies/OrderPolicy.php
@@ -11,43 +11,49 @@ class OrderPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): Response
-    {
-        return $user->role === 'admin' || $user->role === 'responsable_commandes'
-            ? Response::allow()
-            : Response::deny('Vous n\'avez pas les droits pour accéder à cette ressource.');
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Order $order): bool
-    {
-        return $user->role === 'admin' || $user->role === 'responsable_commandes' || $user->id === $order->user_id;
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
+    public function viewAny(User $user): bool
     {
         return true;
     }
 
     /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Order $order): Response
+    {
+        return $user->role === 'admin' || $user->role === 'responsable_commandes' || $user->id === $order->user_id
+            ? Response::allow()
+            : Response::deny('Vous n\'avez pas les droits pour accéder à cette ressource.');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): Response
+    {
+        return $user->role === 'admin' || $user->role === 'responsable_commandes' || $user->role === 'user'
+            ? Response::allow()
+            : Response::deny('Vous n\'avez pas les droits pour créer une commande.');
+    }
+
+    /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Order $order): bool
+    public function update(User $user, Order $order): Response
     {
-        return ($user->role === 'admin' || $user->role === 'responsable_commandes' || $user->id === $order->user_id) && $order->status !== 'Terminée';
+        return ($user->role === 'admin' || $user->role === 'responsable_commandes' || $user->id === $order->user_id) && ($order->status !== 'Terminée' && $order->status !== 'Annulée')
+            ? Response::allow()
+            : Response::deny('Vous n\'avez pas les droits pour modifier cette commande.');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Order $order): bool
+    public function delete(User $user, Order $order): Response
     {
-        return ($user->role === 'admin' || $user->role === 'responsable_commandes' || $user->id === $order->user_id) && $order->status !== 'Terminée';
+        return ($user->role === 'admin' || $user->role === 'responsable_commandes' || $user->id === $order->user_id) && ($order->status !== 'Terminée' && $order->status !== 'Annulée')
+            ? Response::allow()
+            : Response::deny('Vous n\'avez pas les droits pour supprimer cette commande.');
     }
 
     /**
