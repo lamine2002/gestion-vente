28e441e2f1e8650f34a4b9a929ce29cea0d36d6a
Mohamed Lamine NIANG <123456789+odin@users.noreply.github.com>
2024-03-04 00:02:28 +0000

    implementation du show de order

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -3,8 +3,74 @@
 @section('title', 'Details de la commande'.' '.$order->numOrder)
 
 @section('content')
+    @php
+        $status = [
+            'En attente' => 'bg-yellow-200 text-yellow-600',
+            'En cours de traitement' => 'bg-blue-200 text-blue-600',
+            'Expédiée' => 'bg-green-200 text-green-600',
+            'Annulée' => 'bg-red-200 text-red-600',
+        ];
+
+        $status_class = $status[$order->status];
+        $customer = \App\Models\Customer::find($order->customer_id);
+    @endphp
     <div class="flex justify-between mb-8">
-        <a href="{{ route('admin.orders.index') }}" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Retour</a>
+        <a href="{{ route('admin.orders.index') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Retour</a>
+    </div>
+    <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead>
+                            <tr>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Numéro de commande
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Client
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Date
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Status
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <tr>
+                                <td class="px-6 py-4 whitespace-no-wrap">
+                                    <div class="text-sm leading-5 font-medium text-gray-900">{{ $order->numOrder }}</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap">
+                                    <div class="text-sm leading-5 text-gray-900">{{ $customer->firstname.' '.$customer->lastname }}</div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap">
+                                    <div class="text-sm leading-5 text-gray-900">
+                                        @if($order->created_at)
+                                            {{ $order->created_at->format('d/m/Y à H:i') }}
+                                        @else
+                                            Date de création non disponible
+                                        @endif
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap">
+                                    <div class="text-sm leading-5 text-gray-900">
+                                        <span class="py-1 px-3 rounded-full text-xs {{ $status_class }}">{{ $order->status }}</span>
+
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="flex justify-between mt-8">
+        <h2 class="text-2xl font-semibold">Produits Commandes</h2>
     </div>
 
     <div class="flex flex-col">
@@ -15,13 +81,13 @@
                         <thead>
                             <tr>
                                 <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                    ID
-                                </th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                     Produit
                                 </th>
                                 <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                     Quantité
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Prix unitaire
                                 </th>
                             </tr>
                         </thead>
@@ -29,13 +95,13 @@
                             @foreach ($order->products as $product)
                                 <tr>
                                     <td class="px-6 py-4 whitespace-no-wrap">
-                                        <div class="text-sm leading-5 font-medium text-gray-900">{{ $product->id }}</div>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-no-wrap">
-                                        <div class="text-sm leading-5 text-gray-900">{{ $product->name }}</div>
+                                        <div class="text-sm leading-5 font-medium text-gray-900">{{ $product->name }}</div>
                                     </td>
                                     <td class="px-6 py-4 whitespace-no-wrap">
                                         <div class="text-sm leading-5 text-gray-900">{{ $product->pivot->quantity }}</div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap">
+                                        <div class="text-sm leading-5 text-gray-900">{{ $product->price }} €</div>
                                     </td>
                                 </tr>
                             @endforeach
@@ -45,5 +111,103 @@
             </div>
         </div>
     </div>
-@endsection
+    <div class="flex justify-between mt-8">
+        <h2 class="text-2xl font-semibold">Adresse de livraison</h2>
+    </div>
+    <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead>
+                            <tr>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                                    Adresse
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <tr>
+                                <td class="px-6 py-4 whitespace-no-wrap">
+                                    <div class="text-sm leading-5 font-medium text-gray-900">{{ $customer->address }}</div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-between mt-8">
+        <h2 class="text-2xl font-semibold">Total</h2>
+    </div>
+    <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead>
+                            <tr>
+                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 flex justify-end uppercase tracking-wider">
+                                    Total
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <tr>
+                                <td class="px-6 py-4 whitespace-no-wrap flex justify-end">
+                                    <h1 class="text-xl leading-5 font-medium text-gray-900">{{ $order->total }} €</h1>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-between mt-8">
+        <h2 class="text-2xl font-semibold">Actions</h2>
+    </div>
+    <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead>
+                            <tr>
+                                <th class="px-6 py-3 bg-gray-50"></th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <tr>
+                                <td class="px-6 py-4 whitespace-no-wrap">
+                                    <div class="flex items center">
+                                        @if($order->status == 'En attente')
+                                            <form action="{{ route('admin.orders.update', $order) }}" method="POST">
+                                                @csrf
+                                                @method('PUT')
+                                                <input type="hidden" name="status" value="En cours de traitement">
+                                                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Passer en cours de traitement</button>
+                                            </form>
+                                        @elseif($order->status == 'En cours de traitement')
+                                            <form action="{{ route('admin.orders.update', $order) }}" method="POST">
+                                                @csrf
+                                                @method('PUT')
+                                                <input type="hidden" name="status" value="Terminée">
+                                                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Terminée</button>
+                                            </form>
+                                        @endif
 
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
