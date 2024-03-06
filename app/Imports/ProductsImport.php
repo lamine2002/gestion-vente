4154ba7408ba75eb209c2afecddff6e0232311b9
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-03-06 18:50:26 +0000

    Mise en commentaire du code inutile de ProductsImport

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -16,15 +16,15 @@ class ProductsImport implements ToModel, WithHeadingRow
      */
     public function model(array $row)
     {
-        dd($row);
+//        dd($row);
         // Téléchargez l'image à partir de l'URL
-        $imageContents = ($row['photo']);
+//        $imageContents = ($row['photo']);
 //        dd($imageContents);
         // Générer un nom de fichier unique
-        $imageName = uniqid() . '.jpg';
+//        $imageName = uniqid() . '.jpg';
 
         // Enregistrez l'image sur votre serveur
-        Storage::disk('public')->put('products/' . $imageName, $imageContents);
+//        Storage::disk('public')->put('products/' . $imageName, $imageContents);
 
         if ($row['categorie'] === 'Alimentaire') {
             $categoryId = 1;
@@ -41,7 +41,7 @@ class ProductsImport implements ToModel, WithHeadingRow
             'category_id' => $categoryId,
             'price' => $row['prix'],
             'stock' => $row['stock'],
-            'photo' => 'products/' . $imageName,
+            'photo' => $row['photo'],
         ]);
 
     }
