3836cd945cf31f87c84c8376416391b82bf3648b
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-03-06 17:54:19 +0000

    implementation de de ProductsImport.php qui ne marche pas pour l'instant

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -16,19 +16,29 @@ class ProductsImport implements ToModel, WithHeadingRow
      */
     public function model(array $row)
     {
+        dd($row);
         // Téléchargez l'image à partir de l'URL
         $imageContents = file_get_contents($row['photo']);
-
+//        dd($imageContents);
         // Générer un nom de fichier unique
         $imageName = uniqid() . '.jpg';
 
         // Enregistrez l'image sur votre serveur
         Storage::disk('public')->put('products/' . $imageName, $imageContents);
 
+        if ($row['categorie'] === 'Alimentaire') {
+            $categoryId = 1;
+        } else if ($row['categorie'] === 'Cosmetique') {
+            $categoryId = 2;
+        } else {
+            $categoryId = 3;
+        }
+
         // Enregistrez le produit dans votre base de données
         return new Product([
             'name' => $row['nom'],
             'description' => $row['description'],
+            'category_id' => $categoryId,
             'price' => $row['prix'],
             'stock' => $row['stock'],
             'photo' => 'products/' . $imageName,
