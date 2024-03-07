738f1bc8e50dc06337c099d09ab76ae691985b9d
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-03-07 22:13:21 +0000

    Implementation de searchorderrequest

--- app/Http/Requests/Admin/SearchOrderRequest.php
+++ app/Http/Requests/Admin/SearchOrderRequest.php
@@ -11,7 +11,7 @@ class SearchOrderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class SearchOrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'customer_name' => 'nullable|string',
+            'order_number' => 'nullable|string',
+            'order_status' => 'nullable|string',
+            'order_date' => 'nullable|string',
         ];
     }
 }
